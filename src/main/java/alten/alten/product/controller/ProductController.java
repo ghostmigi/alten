9b6d1e9d3f1828c8812c7a620278c16722c468cf
Karim Saber <saber.karim1991@gmail.com>
2025-03-01 19:51:12 +0000

    add cross origine

--- src/main/java/alten/alten/product/controller/ProductController.java
+++ src/main/java/alten/alten/product/controller/ProductController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/products")
 @RequiredArgsConstructor
--- src/main/java/alten/alten/user/controller/UserController.java
+++ src/main/java/alten/alten/user/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
